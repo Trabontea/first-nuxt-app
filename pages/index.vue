ce48a3a3ae7dbcac387e21bd286031fdb1a47dba
Trabontea <danielmbotea@gmail.com>
2022-04-19 23:11:59 +0300

    pagination

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,19 @@
 <template>
-  <Tutorial/>
+  <section>
+    <div
+      class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+    >
+      <h1>Hello i'm first vue/Nuxt app</h1>
+      <!-- <a href="/users">Users</a> -->
+      <nuxt-link to="/users">Users</nuxt-link>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
---
+++ pages/users/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>This is the users page</h1>
+    <input type="text" v-model="userId" />
+    <button @click="onLoadUser">Load User</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: "",
+    };
+  },
+  methods: {
+    onLoadUser() {
+      this.$router.push("/users/" + this.userId);
+    },
+  },
+};
+</script>
